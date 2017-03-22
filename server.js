c3de719b4826ae1efe245d536895d1194061dbc1
gritsbilandris <belandresgretchit1a@gmail.com>
2017-03-22 17:06:36 +0800

    updated server.js

--- server.js
+++ server.js
@@ -7,26 +7,8 @@ app.set('port', (process.env.PORT || 5000));
 //use static files
 app.use(express.static(path.join(__dirname, 'assets')));
 //express routes
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'assets/home.html'));
-});
-app.get('/about', function(req, res){
-res.sendFile(path.join(__dirname, 'assets/about.html'));
-});
-app.get('/about', function(req, res){
-res.sendFile(path.join(__dirname, 'assets/about.html'));
-});
-app.get('/seasonsofglass', function(req, res){
-res.sendFile(path.join(__dirname, 'assets/seasonsofglass.html'));
-});
-app.get('/flowerbud', function(req, res){
-res.sendFile(path.join(__dirname, 'assets/flowerbud.html'));
-});
-app.get('/snowflake', function(req, res){
-res.sendFile(path.join(__dirname, 'assets/snowflake.html'));
-});
-app.get('/awakening', function(req, res){
-res.status(404).sendFile(path.join(__dirname, 'assets/awakening.html'));
+app.get('/main', function(req, res){
+  res.sendFile(path.join(__dirname, 'assets/Template.html'));
 });
 //express server listen
 var server = app.listen(app.get('port'), function(){
